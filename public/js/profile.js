e7914cc01bf64502cdc1f8d26225651575f63719
Chelsea Long <clong6@gmail.com>
2019-03-01 19:21:17 -0700

    profile working

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,27 @@
 console.log("loaded profile.js");
 
+$("#update-button").on("click", function(event) {
+    console.log("clicked update");
+
+    var body = {
+        firstname: $("#firstname").val(),
+        lastname: $("#lastname").val(),
+        username: $("#username").val(),
+        password: $("#password").val(),
+        email: $("#email").val()
+      };
+
+      $.ajax("/api/users/", {
+        type: "PUT",
+        data: JSON.stringify(body),
+        contentType: "application/json"
+      })
+        .done(function() {
+          location.href = "/profile";
+        })
+        .fail(function(jqXHR, textStatus) {
+          alert("error: " + textStatus);
+        });
+    });
+
 
--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -27,6 +27,26 @@ module.exports = function(app) {
     });
   });
 
+  app.put("/api/users", function(req, res) {
+    db.User.findOne({
+      where: {
+        id: req.session.user.id
+      }
+    }).then(function(dbUser) {
+      var body = req.body;
+      dbUser.firstname = body.firstname;
+      dbUser.lastname = body.lastname;
+      dbUser.username = body.username;
+      if (body.password) {
+        dbUser.password = body.password;
+      }
+      dbUser.email = req.body.email;
+      dbUser.save().then(function() {
+        res.json(dbUser);
+      });
+    });
+  });
+
   app.delete("/api/users/:id", function(req, res) {
     db.User.destroy({
       where: {

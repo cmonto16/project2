4c3bcd20cf6c5830ce4dfda1c2585b06cf97cecb
tmullen <tmullen1991@gmail.com>
2019-03-05 11:08:04 -0700

    updating files

--- public/js/index.js
+++ public/js/index.js
@@ -1,49 +1,47 @@
-$(document).ready(function() {
-  console.log("index.js has loaded")
-  $(".modal").modal();
+$("#btn").on("click", function(event) {
+  $("#modal1").modal();
+})
+$("#login-button").on("click", function(event) {
+  console.log("clicked submit");
 
-  $("#login-button").on("click", function(event) {
-    console.log("clicked submit");
+  var body = {
+    username: $("#username").val(),
+    password: $("#password").val()
+  };
 
-    var body = {
-      username: $("#username").val(),
-      password: $("#password").val()
-    };
-
-    $.ajax("/api/sessions", {
-      type: "POST",
-      data: JSON.stringify(body),
-      contentType: "application/json"
+  $.ajax("/api/sessions", {
+    type: "POST",
+    data: JSON.stringify(body),
+    contentType: "application/json"
+  })
+    .done(function() {
+      location.href = "/posts";
     })
-      .done(function() {
-        location.href = "/posts";
-      })
-      .fail(function(jqXHR, textStatus) {
-        alert("error: " + textStatus);
-      });
-  });
+    .fail(function(jqXHR, textStatus) {
+      alert("error: " + textStatus);
+    });
+});
 
-  $("#new-button").on("click", function(event) {
-    console.log("clicked submit");
+$("#new-button").on("click", function(event) {
+  console.log("clicked submit");
 
-    var body = {
-      firstname: $("#new_firstname").val(),
-      lastname: $("#new_lastname").val(),
-      username: $("#new_username").val(),
-      password: $("#new_password").val(),
-      email: $("#new_email").val()
-    };
+  var body = {
+    firstname: $("#new_firstname").val(),
+    lastname: $("#new_lastname").val(),
+    username: $("#new_username").val(),
+    password: $("#new_password").val(),
+    email: $("#new_email").val()
+  };
 
-    $.ajax("/api/users", {
-      type: "POST",
-      data: JSON.stringify(body),
-      contentType: "application/json"
+  $.ajax("/api/users", {
+    type: "POST",
+    data: JSON.stringify(body),
+    contentType: "application/json"
+  })
+    .done(function() {
+      location.href = "/posts";
     })
-      .done(function() {
-        location.href = "/posts";
-      })
-      .fail(function(jqXHR, textStatus) {
-        alert("error: " + textStatus);
-      });
-  });
+    .fail(function(jqXHR, textStatus) {
+      alert("error: " + textStatus);
+    });
 });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,51 +3,6 @@ var db = require("../models");
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
-    res.render("index")
-  });
-
-
-  app.get("/profile", function(req, res) {
-    res.render("profile")
-    if (!req.session.user) {
-      console.log(db.Post[0])
-      db.User.findOne({
-        where: {
-          id: req.session.user.id
-        },
-        include: [db.Post]
-      }).then(function(dbUser) {
-        res.render("profile", {
-          user: db.Post
-        });
-      });
-    }
-  });
-
-  app.get("/posts", function(req, res) {
-      res.render("posts");
-      db.Post.findAll({
-        attributes: ['id', 'title', 'UserID']
-      }).then(function (dbPost) {
-        console.log(dbPost)
-      });
-  });
-
-  app.get("/newpost", function(req, res) {
-    res.render("newpost");
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-};
-
-var db = require("../models");
-
-module.exports = function(app) {
-
-  app.get("/", function(req, res) {
     /*var query = {};
     if (req.query.User_id) {
       query.UserId = req.query.User_id;
@@ -85,6 +40,10 @@ module.exports = function(app) {
 
   app.get("/newpost", function(req, res) {
     res.render("newpost");
+  });
+
+  app.get("/login", function(req, res) {
+    res.render("login");
   });
 
   // Render 404 page for any unmatched routes

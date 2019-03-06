b395d782d052ca4b1f08e10f8068e049d8879409
tmullen <tmullen1991@gmail.com>
2019-03-05 20:46:38 -0700

    added edit for user only

--- public/js/posts.js
+++ public/js/posts.js
@@ -45,7 +45,7 @@ if ($("#user-id").attr("value") === undefined) {
   $(".edit-link").hide();
 } else {
   $(".edit-link").hide();
-  for (i = 1; i < 20; i++) {
+  for (i = 1; i < 1000; i++) {
     if($("#edit"+i).attr('value') === $("#user-id").attr("value")){
       $("#edit"+i).show();
     }
